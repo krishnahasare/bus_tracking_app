2ca726e18d3bf2c7a72153a5e8b878ae93672e10
Sumera01 <sumera.s.shaikh11@gmail.com>
2025-06-10 17:54:45 +0530

    Updated frontent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './components/AdminDashboard';
+import AttendanceDashboard from "./components/AttendanceDashboard";
 import StudentLogs from './components/StudentLogs';
 import SearchLocation from './components/SearchLocation';
-
-function AppLayout() {
-  const location = useLocation();
-
-  return (
-    <div className="max-w-6xl mx-auto mt-8">
-      {/* Show "Go to Bus Location Search" only on home */}
-      {location.pathname === '/' && (
-        <Link to="/searchlocation">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4">
-            Go to Bus Location Search
-          </button>
-        </Link>
-      )}
-
-      {/* Show Back button only on /searchlocation */}
-      {location.pathname === '/searchlocation' && (
-        <Link to="/">
-          <button className="bg-green-500 text-white px-4 py-2 rounded mb-4">
-            ← Back to Student Logs
-          </button>
-        </Link>
-      )}
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <StudentLogs />
-              <hr className="my-6" />
-              <AdminDashboard />
-            </>
-          }
-        />
-        <Route path="/searchlocation" element={<SearchLocation />} />
-      </Routes>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <Router>
-      <AppLayout />
+      <Routes>
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/attendance" element={<AttendanceDashboard />} />
+        <Route path="/searchlocation" element={<SearchLocation />} />
+        <Route path="/student-logs" element={<StudentLogs />} />
+        <Route path="/emergencies-overspeeding" element={<div>Emergencies/Overspeeding Page (To be implemented)</div>} />
+      </Routes>
     </Router>
   );
 }
